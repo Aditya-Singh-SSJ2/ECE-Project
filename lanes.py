b78ae6847ae50a60b30bc6e7a391a179a156995e
Aditya-Mac <aditosh007@gmail.com>
2022-02-17 13:34:51 +0530

    Bug Fix - Lane Detection - Works with left-spotted-lanes without errors

--- lanes.py
+++ lanes.py
@@ -1,6 +1,21 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+
+prev_left_fit_average = prev_right_fit_average = None
+
+def canny(image):
+    gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    blur=cv2.GaussianBlur(gray,(5,5),0)
+    canny=cv2.Canny(blur,50,150)
+    return canny
+
+def region_of_interest(image):
+    height=image.shape[0]
+    polygons=np.array([[(200,height),(1100,height),(550,250)]])
+    mask=np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    masked_image=cv2.bitwise_and(image,mask)
+    return masked_image
 
 def make_coordinates(image,line_parameters):
     slope,intercept=line_parameters
@@ -11,6 +26,7 @@ def make_coordinates(image,line_parameters):
     return np.array([x1,y1,x2,y2])
 
 def average_slope_intercept(image,lines):
+    global prev_left_fit_average, prev_right_fit_average
     left_fit=[]
     right_fit=[]
     for line in lines:
@@ -21,26 +37,22 @@ def average_slope_intercept(image,lines):
         if slope<0:
             left_fit.append((slope,intercept))
         else:
-            right_fit.append((slope,intercept))
+            right_fit.append((slope,intercept))        
 
     left_fit_average=np.average(left_fit,axis=0)
     right_fit_average=np.average(right_fit,axis=0)
     print(left_fit_average,'left')
     print(right_fit_average,'right')
-    left_line=make_coordinates(image,left_fit_average)
-    right_line=make_coordinates(image,right_fit_average)
+    try:
+        left_line=make_coordinates(image,left_fit_average)
+        right_line=make_coordinates(image,right_fit_average)
+        prev_left_fit_average = left_fit_average
+        prev_right_fit_average = right_fit_average
+    except:
+        left_line=make_coordinates(image,prev_left_fit_average)
+        right_line=make_coordinates(image,prev_right_fit_average)
     return np.array([left_line,right_line])
 
-
-
-
-
-
-def canny(image):
-    gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
-    blur=cv2.GaussianBlur(gray,(5,5),0)
-    canny=cv2.Canny(blur,50,150)
-    return canny
 
 def display_lines(image,lines):
     line_image=np.zeros_like(image)
@@ -51,37 +63,24 @@ def display_lines(image,lines):
     return line_image
 
 
-def region_of_interest(image):
-    height=image.shape[0]
-    polygons=np.array([[(200,height),(1100,height),(550,250)]])
-    mask=np.zeros_like(image)
-    cv2.fillPoly(mask,polygons,255)
-    masked_image=cv2.bitwise_and(image,mask)
-    return masked_image
-
-
-#image=cv2.imread('test_image.jpg')
-#lane_image=np.copy(image)
-#canny_image=canny(lane_image)
-#cropped_image=region_of_interest(canny_image)
-#lines=cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
-#averaged_lines=average_slope_intercept(lane_image,lines)
-#line_image=display_lines(lane_image,averaged_lines)
-#combo_image=cv2.addWeighted(lane_image,0.9,line_image,1,1)
-#cv2.imshow('result',combo_image)
-#cv2.waitKey(0)
-
 cap=cv2.VideoCapture("test2.mp4")
 while(cap.isOpened()):
     _,frame=cap.read()
     canny_image=canny(frame)
+    
     cropped_image=region_of_interest(canny_image)
+    
     lines=cv2.HoughLinesP(cropped_image,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
     averaged_lines=average_slope_intercept(frame,lines)
+    
     line_image=display_lines(frame,averaged_lines)
+    
     combo_image=cv2.addWeighted(frame,0.9,line_image,1,1)
+    
     cv2.imshow('result',combo_image)
-    if cv2.waitKey(1) & 0xFF==ord(27):  # ESC KEY to close
+    cv2.imshow('cannyImage', canny_image)
+    cv2.imshow("ROI-Image", cropped_image)
+    if cv2.waitKey(1) & 0xFF==27:  # ESC KEY to close
         break
 cv2.release()
 cv2.destroyAllWindows()
